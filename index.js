0423e606fae5c80bfcafc62a8212866ea5f4063b
Tanuj Garg <tanujgarg@y7mail.com>
2019-04-20 20:48:27 +0530

    completed

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const server = require("http").Server(app)
 const io = require("socket.io")(server, {origins: process.env.ORIGIN})
 const PORT = process.env.PORT || 3000
 
+let users = []
+
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`socket server is running on port ${PORT}`)
 })
 
 io.on('connection', (socket) => {
-  io.emit("count", io.engine.clientsCount)
-  socket.on('disconnect', () => {
+
+  socket.on('login', (data) => {
+    socket.username = data.username
+    users.includes(data.username)
+      ? console.log("already")
+      : users.push(data.username)
+    io.emit("users", users)
     io.emit("count", io.engine.clientsCount)
+  })
+  socket.on('disconnect', (id) => {
+    io.emit("count", io.engine.clientsCount)
+    io.emit("users", users.filter(user => user !== socket.username))
   })
 })
